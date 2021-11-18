29165e35b2a108ed97733f3db34b29930ff92150
Ahmet Faruk ASLANTAŞ <ahmetf.aslantas@gmail.com>
2021-11-18 18:52:24 +0300

    Home Page is now its own file. Drawer added.

---
+++ lib/pages/home.dart
@@ -0,0 +1,63 @@
+import 'package:crowdate/model/event.dart';
+import 'package:crowdate/view/eventpreview.dart';
+import 'package:crowdate/viewmodel/event.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {},
+          )
+        ],
+      ),
+      body: Center(),
+      drawer: Drawer(
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            UserAccountsDrawerHeader(
+                accountName: Text("Name"), // TODO: Replace with user name.
+                accountEmail: Text("Email"), // TODO: Replace with user email.
+                currentAccountPicture: CircleAvatar(
+                  backgroundImage: AssetImage("assets/icon/Icon-512.png"), // TODO: Replace with network image.
+                )),
+            Expanded(child: Container()),
+            Divider(
+              color: Colors.black54,
+              height: 1,
+            ),
+            ListTile(
+              title: Row(
+                children: [
+                  Icon(Icons.logout),
+                  Padding(
+                    padding: EdgeInsets.only(left: 8),
+                    child: Text("Logout"),
+                  )
+                ],
+              ),
+              onTap: () {
+                ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Logged out!"),));
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
